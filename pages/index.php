84ae99ec2bb2c04239d751bbad2da9aa76dd5f3c
Nikita Nitichevski <nikita.n@nititech.de>
2024-03-08 21:49:49 +0100

    updated styling

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,10 @@ $layout = new HTML(title: 'PHP via Vite');
 
 <div class="flex flex-col items-center text-2xl">
 	<?php include('partials/vite-name.php'); ?>
+
 	<div>+</div>
+
 	<img src="/logo.svg" class="w-20" />
-	<a href="<%= BASE %>/" class="mt-10">Home-Link</a>
+
+	<a href="<%= BASE %>/" class="mt-10 text-base underline text-sky-800 hover:no-underline">Home</a>
 </div>
