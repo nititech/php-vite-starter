916c47f2dd9bccce74829e2c249ccc4f053794a5
Nikita Nitichevski <nikita.n@nititech.de>
2024-07-13 10:59:44 +0200

    added auto require for composer

--- system/main.php
+++ system/main.php
@@ -3,4 +3,10 @@ define('ROOT', dirname(__DIR__));
 
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . ROOT);
 
+function require_existing(string $path) {
+	file_exists($path) && require_once($path);
+}
+
+require_existing('vendor/autoload.php');
+
 require_once('partials/layouts/HTML.php');
