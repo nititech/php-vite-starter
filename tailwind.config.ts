3d05e576d67d0e93edb52828f453186743727b22
Nikita Nitichevski <nikita.n@nititech.de>
2024-08-09 15:21:14 +0200

    updated packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./**/*.{php,html}', './src/**/*.{js,jsx,ts,tsx,svg}'],
+	content: [
+		'./pages/**/*.{php,html}',
+		'./partials/**/*.{php,html}',
+		'./src/**/*.{js,jsx,ts,tsx,svg}',
+	],
 	theme: {
 		extend: {},
 	},
